d175afb0dcc52142b79f755cd9511313b86ca9fc
zaifeng <20012382@qq.com>
2015-07-20 22:10:30 +0800

    add binary search usage

---
+++ interview/binary_search.php
@@ -0,0 +1,25 @@
+<?php
+$arr = array(1,2,3,4,5,6,7,8,9,10,11,12,13,14);
+
+function binary_search($ar , $search_key)
+{
+    $low = 0 ;
+    $high = count($ar)-1 ;
+    while($low <= $high)
+    {
+        $mid = intval(($low + $high)/2);
+        if($ar[$mid] > $search_key){
+            $high = $mid - 1 ;
+        }else if($ar[$mid] == $search_key){
+            return $mid ;
+        }else{
+            $low = $mid + 1;
+        }
+    }
+    if($low > $high){
+        return -1 ;
+    }
+    
+}
+
+var_dump(binary_search($arr , 25));
