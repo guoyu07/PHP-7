cd54a13bbc97b7365bb82925caf6e50dfb0e785f
zaifeng <20012382@qq.com>
2015-08-18 14:10:03 +0800

    add TC ppt

--- extension.php
+++ extension.php
@@ -1,21 +1,7 @@
 <?php
-$br = (php_sapi_name() == "cli")? "":"<br>";
-
 if(!extension_loaded('extstrcat')) {
-    dl('extstrcat.' . PHP_SHLIB_SUFFIX);
+ dl('extstrcat.' . PHP_SHLIB_SUFFIX);
 }
-$module = 'extstrcat';
-$functions = get_extension_funcs($module);
-echo "Functions available in the test extension:$br\n";
-foreach($functions as $func) {
-    echo $func."$br\n";
-}
-echo "$br\n";
-$function = 'confirm_' . $module . '_compiled';
-if (extension_loaded($module)) {
-    $str = $function($module);
-} else {
-    $str = "Module $module is not compiled into PHP";
-}
-echo "$str\n";
+$ret=extstrcat('testarg1111',1234);
+echo $ret;
 ?>
--- test.php
+++ test.php
@@ -11,3 +11,23 @@ $b  = $a1 + $a2 ;
 
 $c  = array_merge($a1 , $a2);
 var_dump($b,$c);
+
+$arr = array();
+
+var_dump($arr > 0);
+
+$blackList = array('abc','acd','bas','kkk') ;
+$adv = "Hello world!!!";
+$isBlack = false ;
+foreach($blackList as $black){
+    if(strpos($black , $adv) !== false){
+        $isBlack = true ;
+        break ;
+    }
+}
+
+if ($isBlack) {
+    
+} else {
+    
+}
