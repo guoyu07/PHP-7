9723389f94d257bed923a9707df8a0f1af6d4981
zaifeng <20012382@qq.com>
2015-09-07 18:16:35 +0800

    add practise

--- test.php
+++ test.php
@@ -1,33 +1,4 @@
 <?php
-$arr = array();
-$a = count($arr) ;
-
-var_dump($a);
-
-
-$a1 = array(1,2,3,25,31);
-$a2 = array(2,16,20,83,90,100);
-$b  = $a1 + $a2 ;
-
-$c  = array_merge($a1 , $a2);
-var_dump($b,$c);
-
-$arr = array();
-
-var_dump($arr > 0);
-
-$blackList = array('abc','acd','bas','kkk') ;
-$adv = "Hello world!!!";
-$isBlack = false ;
-foreach($blackList as $black){
-    if(strpos($black , $adv) !== false){
-        $isBlack = true ;
-        break ;
-    }
-}
-
-if ($isBlack) {
-    
-} else {
-    
-}
+    echo YAF_VERSION,YAF_ENVIRON,"\n";
+    $arr = array("aabcc" => "Hello world");
+    var_dump(http_build_query($arr,'','',PHP_QUERY_RFC3986));
