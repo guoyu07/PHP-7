29c26ea4cbbb756bfbc952c6555b52702b047bb6
zaifeng <20012382@qq.com>
2015-09-10 13:21:29 +0800

    add practise of array + and array_merge

--- test.php
+++ test.php
@@ -1,4 +1,21 @@
 <?php
-    echo YAF_VERSION,YAF_ENVIRON,"\n";
-    $arr = array("aabcc" => "Hello world");
-    var_dump(http_build_query($arr,'','',PHP_QUERY_RFC3986));
+    $a = array(1,2);
+    $b = array(2,3,4);
+    var_export($a ) ;
+    var_export($b) ;
+    echo "a+b = \n\n";
+    var_dump($a+$b);
+
+    echo "array_merge :\n\n" ;
+    var_dump(array_merge($a ,$b));   
+
+    echo "\n";
+
+    $c = array('e' , 'f','p');
+    $d = array('g', 'h','q');
+    var_export($c );
+    var_export($d);
+    echo "c+d \n";
+    var_dump($c+$d);
+    echo "array_merge:\n";
+    var_dump(array_merge($c,$d));
