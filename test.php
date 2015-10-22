60d3a92bfe2dd45ee735e00ee93ec6f09fb48748
zaifeng <20012382@qq.com>
2015-10-22 17:35:19 +0800

    add new test

--- test.php
+++ test.php
@@ -1,21 +1,3 @@
 <?php
-    $a = array(1,2);
-    $b = array(2,3,4);
-    var_export($a ) ;
-    var_export($b) ;
-    echo "a+b = \n\n";
-    var_dump($a+$b);
-
-    echo "array_merge :\n\n" ;
-    var_dump(array_merge($a ,$b));   
-
-    echo "\n";
-
-    $c = array('e' , 'f','p');
-    $d = array('g', 'h','q');
-    var_export($c );
-    var_export($d);
-    echo "c+d \n";
-    var_dump($c+$d);
-    echo "array_merge:\n";
-    var_dump(array_merge($c,$d));
+    $str = '100000000000001000200004110000' ;
+    echo substr($str , 14 , -6) ; 
