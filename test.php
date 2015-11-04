ed7968f76086b6f4b595dd666a9663d6896f25ff
zaifeng <20012382@qq.com>
2015-11-04 11:27:52 +0800

    test modified

--- test.php
+++ test.php
@@ -1,3 +1,7 @@
 <?php
-    $str = '100000000000001000200004110000' ;
-    echo substr($str , 14 , -6) ; 
+$file_name = "喝啊大叔啊是的V在程序执行阿斯顿爱仕达阿斯顿发生的发生大的事dwfasdfadsafasd";
+if(mb_strlen($file_name) > 10) {
+    $file_name = mb_substr($file_name, 0 , 3) .'...'.mb_substr($file_name, -3) ;
+}
+echo $file_name ;
+
