8095e2fca809cab43c671d5268d3911bc9f97d73
zaifeng <20012382@qq.com>
2016-07-22 18:22:24 +0800

    add new question

---
+++ test.php
@@ -0,0 +1,7 @@
+<?php
+$checksum[] = sprintf("%u",crc32('1'));
+$checksum[] = sprintf("%u",crc32('2'));
+$checksum[] = sprintf("%u",crc32('3'));
+$checksum[] = sprintf("%u",crc32('4'));
+$checksum[] = sprintf("%u",crc32('5'));
+var_dump($checksum);
